05a2de83d4ead1f289fa0565af3ff6cc356f1e10
shotslinger-debug <shotslinger01@gmail.com>
2026-04-13 22:01:40 -0600

    estructura de carpetas y logica de carga CSV inicial

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(page_title="Probabilidad y estadistica - 2A", layout="centered")
 st.title("Probabilidad y estadistica - 2A")
 st.write("Bienvenido a tu aplicacion basica de Streamlit.")
+
+archivo_csv = st.file_uploader("Sube un archivo CSV", type=["csv"])
+
+if archivo_csv is not None:
+    datos = pd.read_csv(archivo_csv)
+    st.success("Archivo cargado con exito")
+    st.dataframe(datos.head(5))
